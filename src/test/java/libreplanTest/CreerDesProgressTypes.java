1ebb00e93a5111bc50a446e1dcbe4c8185a1a47d
Formation <Formation@192.168.2.26>
2018-02-23 09:52:41 +0100

    ModifTorunOnGridSelenium

--- src/test/java/libreplanTest/CreerDesProgressTypes.java
+++ src/test/java/libreplanTest/CreerDesProgressTypes.java
@@ -2,20 +2,48 @@ package libreplanTest;
 
 import static org.junit.Assert.assertTrue;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.PageFactory;
 
 public class CreerDesProgressTypes {
-	WebDriver driver;
+	//WebDriver driver;
+	RemoteWebDriver driver;
 	@Before
 	//COnnexion à l'application
-	public void setup() {
-		driver = new FirefoxDriver();
+	public void setup() throws MalformedURLException {
+		//driver = new FirefoxDriver();
+		//WebDriver browser=null;  
+		//RemoteWebDriver driver;
+		String nav =  System.getProperty("browser");
+		//Firefox's geckodriver requires you to specify its location.  
+		DesiredCapabilities cap = new DesiredCapabilities();
+		cap.setBrowserName(nav);
+		
+		cap.setPlatform(org.openqa.selenium.Platform.WINDOWS);
+		URL url_hub = new URL("http://192.168.2.26:4444/wd/hub");
+
+		driver = new RemoteWebDriver(url_hub,cap);
+		/*
+		if(nav.equals("chrome")) {
+			browser = new ChromeDriver();  			
+		}
+		if(nav.equals("firefox")) {
+			FirefoxOptions options = new FirefoxOptions().setProfile(new FirefoxProfile());
+			options.addPreference("browser.tabs.remote.autostart",  false);
+			System.setProperty("webdriver.gecko.driver","C:\\FORMATION\\installeurs\\Selenium\\geckodriver.exe");   
+			browser = new FirefoxDriver(options);  			
+		}		*/				
+		
 		driver.get("http://localhost:8085/libreplan");
 	}
 	
--- src/test/java/libreplanTest/CreerUneMachine.java
+++ src/test/java/libreplanTest/CreerUneMachine.java
@@ -2,19 +2,49 @@ package libreplanTest;
 
 import static org.junit.Assert.assertTrue;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.PageFactory;
 
 public class CreerUneMachine {
 
-	WebDriver driver;
+	//WebDriver driver;
+	RemoteWebDriver driver;
 	@Before
 	//Connexion à l'application
-	public void setup() {
-		driver = new FirefoxDriver();
+	public void setup() throws MalformedURLException {
+		//driver = new FirefoxDriver();
+		//WebDriver browser=null;  
+		
+		String nav =  System.getProperty("browser");
+		//Firefox's geckodriver requires you to specify its location.  
+		DesiredCapabilities cap = new DesiredCapabilities();
+		cap.setBrowserName(nav);
+		
+		cap.setPlatform(org.openqa.selenium.Platform.WINDOWS);
+		URL url_hub = new URL("http://192.168.2.26:4444/wd/hub");
+
+		driver = new RemoteWebDriver(url_hub,cap);
+		/*
+		if(nav.equals("chrome")) {
+			browser = new ChromeDriver();  			
+		}
+		if(nav.equals("firefox")) {
+			FirefoxOptions options = new FirefoxOptions().setProfile(new FirefoxProfile());
+			options.addPreference("browser.tabs.remote.autostart",  false);
+			System.setProperty("webdriver.gecko.driver","C:\\FORMATION\\installeurs\\Selenium\\geckodriver.exe");   
+			browser = new FirefoxDriver(options);  			
+		}		*/			
+		
+		
+		
 		driver.get("http://localhost:8085/libreplan");
 	}
 @Test
